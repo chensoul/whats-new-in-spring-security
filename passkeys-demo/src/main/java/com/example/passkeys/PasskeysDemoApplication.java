989321acc92828540d4734057ecac5b6618cae95
Andreas Falk <info@andreas-falk.de>
2024-09-19 15:04:23 +0200

    add passkeys demo

---
+++ passkeys-demo/src/main/java/com/example/passkeys/PasskeysDemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.passkeys;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PasskeysDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PasskeysDemoApplication.class, args);
+	}
+
+}
---
+++ passkeys-demo/src/main/java/com/example/passkeys/WebSecurityConfiguration.java
@@ -0,0 +1,58 @@
+package com.example.passkeys;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.HeadersConfigurer.HstsConfig;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+import static org.springframework.security.config.Customizer.withDefaults;
+import static org.springframework.security.config.annotation.web.configurers.WebauthnConfigurer.webauthn;
+
+@Configuration
+@EnableWebSecurity
+public class WebSecurityConfiguration {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http.authorizeHttpRequests(
+                        authorizeRequests -> {
+                            authorizeRequests.requestMatchers("/login/**", "/message", "/error").permitAll();
+                            authorizeRequests.anyRequest().authenticated();
+                        }
+                )
+                .headers(headers -> headers.httpStrictTransportSecurity(HstsConfig::disable))
+                .httpBasic(withDefaults())
+                .formLogin(withDefaults())
+                .with(webauthn(), (passkeys) -> passkeys
+                        .rpName("Spring Security Relying Party")
+                        .rpId("server.local")
+                        .allowedOrigins("https://server.local:8443")
+                );
+        return http.build();
+    }
+
+    @Bean
+    public UserDetailsService userDetailsService() {
+        return new InMemoryUserDetailsManager(
+                User
+                        .builder()
+                        .username("user")
+                        .password("secret")
+                        .passwordEncoder(passwordEncoder()::encode)
+                        .roles("USER")
+                        .build()
+        );
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return PasswordEncoderFactories.createDelegatingPasswordEncoder();
+    }
+}
